8c44b63a228582f9141797eb4d0c7cba6cd17059
Andrey Kozyrev <andreyk@edifecs.com>
2013-01-27 15:37:03 -0800

    init

---
+++ src/main/java/edu/ak/Application.java
@@ -0,0 +1,12 @@
+package edu.ak;
+
+/**
+ * User: AndreyK
+ * Date: 1/27/13
+ * Time: 3:33 PM
+ */
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
