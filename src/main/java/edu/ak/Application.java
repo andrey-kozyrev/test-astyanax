84a7ef37759908857303b5f4b4d3110e83e547ea
Andrey Kozyrev <andreyk@edifecs.com>
2013-01-27 21:19:16 -0800

    Simplistic performance test done.

--- src/main/java/edu/ak/Application.java
+++ src/main/java/edu/ak/Application.java
@@ -1,11 +1,18 @@
 package edu.ak;
 
 import com.netflix.astyanax.Keyspace;
+import com.netflix.astyanax.connectionpool.OperationResult;
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
 import com.netflix.astyanax.ddl.KeyspaceDefinition;
 import edu.ak.dao.HtcRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * User: AndreyK
@@ -13,18 +20,83 @@ import org.slf4j.LoggerFactory;
  * Time: 3:33 PM
  */
 public class Application {
+
+	public static class Runner implements Runnable {
+
+		private final static String TEXT;
+
+		private final HtcRepository _repository;
+
+		static {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 100; ++i) {
+				sb.append("The Rows object returned by the query transparently paginates through all rows in the column family. Since queries to the keyspace are actually done through the iteration it is necessary to set an ExceptionCallback for your application to handle the exceptions. Return true from the callback to retry or false to exit the iteration loop.");
+			}
+			TEXT = sb.toString();
+		}
+
+		public Runner(HtcRepository repository) {
+			assert repository != null;
+			_repository = repository;
+		}
+
+		@Override
+		public void run() {
+			Logger logger = LoggerFactory.getLogger(Application.class);
+			try {
+				final int count = 10;
+				long latency = 0;
+				Keyspace keyspace = _repository.getKeyspace();
+				for (int n = 0; n < count; ++n) {
+					for (int m = 0; m < count; ++m) {
+						String key = String.format("USER%d:USER%d", n, m);
+						for (int i = 0; i < count; ++i) {
+							Date column = new Date();
+							latency += keyspace.prepareColumnMutation(HtcRepository.CF_TRACKS, key, column).putValue(TEXT, null).execute().getLatency(TimeUnit.MICROSECONDS);
+						}
+					}
+				}
+				logger.debug(String.format("Per-thread insert average: %d milliseconds", latency / count / count / count / 1000));
+			} catch (ConnectionException e) {
+				logger.error("Oops...", e);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Logger logger = LoggerFactory.getLogger(Application.class);
 		logger.info("Started");
 		HtcRepository repository = new HtcRepository();
 		try {
-			repository.init();
-			Keyspace keyspace = repository.getKeyspace();
+			repository.start();
 
+			final int count = 20;
+			List<Thread> runners = new ArrayList<Thread>(count);
 
+			long millis = System.currentTimeMillis();
+
+			for (int i = 0; i < count; ++i) {
+				Thread t = new Thread(new Runner(repository));
+				runners.add(t);
+				t.start();
+			}
+
+			for (Thread t: runners) {
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+					logger.error("Oops...", e);
+				}
+			}
+
+			double delta = System.currentTimeMillis() - millis;
+
+			logger.info(String.format("Overall insert average: %f milliseconds", delta / 1000 / count));
 
 		} catch (ConnectionException e) {
 			logger.error("Oops...", e);
 		}
+
+		repository.stop();
 	}
 }
--- src/main/java/edu/ak/dao/HtcRepository.java
+++ src/main/java/edu/ak/dao/HtcRepository.java
@@ -9,13 +9,18 @@ import com.netflix.astyanax.connectionpool.OperationResult;
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
 import com.netflix.astyanax.connectionpool.impl.ConnectionPoolConfigurationImpl;
 import com.netflix.astyanax.connectionpool.impl.CountingConnectionPoolMonitor;
+import com.netflix.astyanax.ddl.ColumnFamilyDefinition;
 import com.netflix.astyanax.ddl.KeyspaceDefinition;
 import com.netflix.astyanax.ddl.SchemaChangeResult;
 import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
+import com.netflix.astyanax.model.ColumnFamily;
+import com.netflix.astyanax.serializers.DateSerializer;
+import com.netflix.astyanax.serializers.StringSerializer;
 import com.netflix.astyanax.thrift.ThriftFamilyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -30,34 +35,57 @@ public class HtcRepository {
 	private static final Logger logger = LoggerFactory.getLogger(HtcRepository.class);
 	private final AstyanaxContext<Cluster> _clusterCtx;
 
-	private final static String KEYSPACE_NAME_TEST = "test";
-	private Keyspace _keyspace;
+	private Keyspace ks_test;
+	private final static String KS_NAME_TEST = "test";
+	private final Map<String, Object> KS_SETTINGS_TEST = new ImmutableMap.Builder<String, Object>()
+		.put("strategy_options", new ImmutableMap.Builder<String, Object>()
+				.put("replication_factor", "1").build()
+		)
+		.put("strategy_class", "SimpleStrategy")
+		.build();
+
+	private final static String CF_NAME_TRACKS = "tracks";
+	public final static ColumnFamily<String, Date> CF_TRACKS = ColumnFamily.newColumnFamily(
+		CF_NAME_TRACKS,
+		StringSerializer.get(),
+		DateSerializer.get()
+	);
+	private final static Map<String, Object> CF_SETTINGS_TRACKS = new ImmutableMap.Builder<String, Object>()
+		.put("comparator_type", "DateType")
+		.put("key_validation_type", "DateType")
+		.put("default_validation_class", "UTF8Type")
+		.build();
+
 
 	public Keyspace getKeyspace() throws ConnectionException {
-		if (_keyspace == null) {
+		if (ks_test == null) {
 			ensureKeyspace();
 		}
-		return _keyspace;
+		return ks_test;
 	}
 
 	public HtcRepository() {
 		_clusterCtx = new AstyanaxContext.Builder()
 			.forCluster("htc")
 			.withAstyanaxConfiguration(new AstyanaxConfigurationImpl()
-					.setDiscoveryType(NodeDiscoveryType.NONE)
+				.setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE)
 			)
 			.withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("htc")
-					.setPort(9160)
-					.setMaxConnsPerHost(3)
-					.setSeeds("ENCTADB")
+				.setPort(9160)
+				.setMaxConnsPerHost(10)
+				.setSeeds("ENCTADB, ENCTSDB, ENCTSAPP1, ENCTSAPP2, ENCTAAPP")
 			)
 			.withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
 			.buildCluster(ThriftFamilyFactory.getInstance());
-		_clusterCtx.start();
 	}
 
-	public void init() throws ConnectionException {
+	public void start() throws ConnectionException {
+		_clusterCtx.start();
 		ensureKeyspace();
+	}
+
+	public void stop() {
+		_clusterCtx.shutdown();
 	}
 
 	private KeyspaceDefinition findKeyspaceDefinition(String name) throws ConnectionException {
@@ -73,18 +101,18 @@ public class HtcRepository {
 	}
 
 	private void ensureKeyspace() throws ConnectionException {
-		_keyspace = _clusterCtx.getEntity().getKeyspace(KEYSPACE_NAME_TEST);
-		KeyspaceDefinition keyspaceDefinition = findKeyspaceDefinition(KEYSPACE_NAME_TEST);
+		ks_test = _clusterCtx.getEntity().getKeyspace(KS_NAME_TEST);
+		KeyspaceDefinition keyspaceDefinition = findKeyspaceDefinition(KS_NAME_TEST);
 		if (keyspaceDefinition == null) {
-			logger.info(String.format("Keyspace '%s' does not exist.", KEYSPACE_NAME_TEST));
-			Map<String, Object> settings = new ImmutableMap.Builder<String, Object>()
-				.put("strategy_options", new ImmutableMap.Builder<String, Object>()
-					.put("replication_factor", "1").build()
-				)
-				.put("strategy_class", "SimpleStrategy")
-			.build();
-			OperationResult<SchemaChangeResult> opr = _keyspace.createKeyspace(settings);
-			logger.info(String.format("Keyspace '%s' created in %d milliseconds.", KEYSPACE_NAME_TEST, opr.getLatency(TimeUnit.MILLISECONDS)));
+			OperationResult<SchemaChangeResult> opr = ks_test.createKeyspace(KS_SETTINGS_TEST);
+			logger.info(String.format("Keyspace '%s' created in %d milliseconds.", KS_NAME_TEST, opr.getLatency(TimeUnit.MILLISECONDS)));
+			keyspaceDefinition = ks_test.describeKeyspace();
+		}
+
+		ColumnFamilyDefinition cfd = keyspaceDefinition.getColumnFamily("tracks");
+		if (cfd == null) {
+			OperationResult<SchemaChangeResult> opr = ks_test.createColumnFamily(CF_TRACKS, CF_SETTINGS_TRACKS);
+			logger.info(String.format("Column family '%s' created in %d milliseconds.", CF_NAME_TRACKS, opr.getLatency(TimeUnit.MILLISECONDS)));
 		}
 	}
 }
